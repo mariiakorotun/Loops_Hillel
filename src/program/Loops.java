725b4b38fb4ee683baf4d16e225189d85e1953ed
MariiaKorotun <kb221_kmo@student.ztu.edu.ua>
2023-11-06 22:40:51 +0200

    Loops_Hillel_modified

--- src/program/Loops.java
+++ src/program/Loops.java
@@ -3,9 +3,10 @@ package program;
 public class Loops {
     public static void main(String[] args) {
         int sumOfNumbers = 0;
+        int counter = 1;
 
         for(int i = 1; i<=6; ++i){
-            System.out.println("Num is " + i + ", sum is " + (sumOfNumbers += i));
+            System.out.println((counter++) + ") Num is " + i + ", sum is " + (sumOfNumbers += i));
         }
         System.out.println("---------------------------------");
         System.out.println("Sum of numbers is " + sumOfNumbers);
